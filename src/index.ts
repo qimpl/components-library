7e6cfcbccfb19add0412ffdad17cec556ca5b380
Jérôme Pogeant <p-jerome@hotmail.fr>
2021-02-26 22:34:47 +0100

    feat(global): export all components types and variants (#19)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 import Theme from './theme/theme';
 
 export { Theme };
-export { Anchor, Text, Title, Button } from './components';
+export {
+  Anchor,
+  AnchorProps,
+  Button,
+  ButtonProps,
+  Text,
+  TextProps,
+  TextVariant,
+  Title,
+  TitleProps,
+  TitleVariant,
+} from './components';
