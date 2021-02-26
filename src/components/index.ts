962fe7fcbef42d6d6dc5cea02811652be77eee5b
Jérôme Pogeant <p-jerome@hotmail.fr>
2021-02-26 22:31:24 +0100

    feat(global): export all components types and variants

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
-import Anchor from './anchor/anchor';
-import Button from './button/button';
-import Text from './text/text';
-import Title from './title/title';
+import Anchor, { AnchorProps } from './anchor/anchor';
+import Button, { ButtonProps } from './button/button';
+import Text, { TextProps, TextVariant } from './text/text';
+import Title, { TitleProps, TitleVariant } from './title/title';
 
-export { Anchor, Text, Title, Button };
+export { Anchor, AnchorProps, Button, ButtonProps, Text, TextProps, TextVariant, Title, TitleProps, TitleVariant };
